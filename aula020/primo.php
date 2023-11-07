e17fe6414f878a14d304345418040eb228676888
Esdras Furmanovicz <128192976+esdrasfurmanovicz@users.noreply.github.com>
2023-11-07 15:54:01 -0300

    fibo e primo

--- aula020/primo.php
+++ aula020/primo.php
@@ -1,14 +1,43 @@
 <?php
-
-function primo($p){
-    $sequencia = array();
-    $sequencia[0] = 2
-
-    for($i = 2; $i < $p; $i++){
-        
+/*
+    function primo($num) {
+        if ($num < 2) return false;
+        for ($i = 2; $i <= sqrt($num); $i++) {
+            if ($num % $i == 0) return false;
+        }
+        return true;
     }
 
-    return $sequencia[$p-1];
-} 
+    function nthPrimo($n) {
+        $count = 0;
+        $num = 2;
+        while ($count < $n) {
+            if (primo($num)) $count++;
+            $num++;
+        }
+        return $num - 1;
+    }
 
+    echo nthPrimo(10); // 29
+*/
+
+    function numero_primo($posicao){
+        $primos = array(2);
+
+        $n = 3;
+        while(count($primos) < $posicao){
+            $e_primo = true;
+            foreach($primos as $primo){
+                if($n%$primo == 0){
+                    $e_primo = false;
+                }
+            }
+            if($e_primo){
+                $primos[] = $n;
+            }
+            $n++;
+        }
+        return $primos[$posicao - 1];
+    }
+    echo numero_primo(10);
 ?>
---
+++ aula021/formulario.php
@@ -0,0 +1,7 @@
+<?php
+
+    echo "Nome: ".$_POST["inome"];
+    echo "<br>";
+    echo "Email: ".$_POST["imail"];
+
+?>
